3eaeeba7a317df39f448c11d74eeec31138f8bd0
Wildan Kurniadi <danprogramming10@gmail.com>
2018-09-21 18:55:16 +0700

    Learning Request and Response

--- apps.js
+++ apps.js
@@ -4,9 +4,21 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  console.log(req.url);
+
+  if(req.url === '/about'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('I\'m an About Page\n');
+  }else if(req.url === '/contact'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('I\'m an Contact Page\n');
+  }else{
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Sorry Page Not Found, try access our homesite. \n');
+  }
 });
 
 server.listen(port, hostname, () => {
