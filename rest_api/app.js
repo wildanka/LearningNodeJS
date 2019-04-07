9d6d6e4b931d5cad712a838f27ed097310743deb
WildanKurniadi <danprogramming10@gmail.com>
2019-04-07 22:00:29 +0700

    REST API #7 - deploy the CORS Header

--- rest_api/app.js
+++ rest_api/app.js
@@ -13,6 +13,25 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//CORS
+/**
+ * why did we never encounter CORS error when we use postman?
+ * POSTMAN is just a testing tools, it's not a browser.
+ * keep it mind that CORS error are security mechanism and forced by the browsers, thats why we can override them with headers, the browsers then knows to ignore that
+ */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //if you want to protect your api from another webpages, this will work, but you can still get accessed from tools like that (POSTMAN).
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next(); //this will called if the method not immediately returning due to us getting options request, so that the other routes can take over
+});
+
 /**
  * any incoming request has to go through app.use method
  * next is a function which can be used to move the request to the next middleware inline
