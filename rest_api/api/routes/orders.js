536f9a6d42041d5517c016fa01e83213b42f0b57
WildanKurniadi <danprogramming10@gmail.com>
2019-04-07 20:51:22 +0700

    REST API #5 - Create an Error Hnadler (404)

--- rest_api/api/routes/orders.js
+++ rest_api/api/routes/orders.js
@@ -1,16 +1,45 @@
 const express = require('express'); //setup our routes to express
 const router = express.Router(); //setup the express router
 
-//create our first method
+/**
+ * Fetch the orderlist
+ * method : GET
+ */
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'orders were fetched'
   });
 });
 
+/**
+ * Create a new order
+ * method : POST
+ */
 router.post('/', (req, res, next) => {
   res.status(201).json({
     message: 'successfully created an order'
+  });
+});
+
+/**
+ * Get details of specific order id
+ * method : GET
+ */
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'successfully created an order',
+    orderId: req.params.orderId
+  });
+});
+
+/**
+ * Get details of specific order id
+ * method : GET
+ */
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'order with id ' + req.params.orderId + ' successfully deleted',
+    orderId: req.params.orderId
   });
 });
 //export the router so it can be used by other files (like what we have done to our app.js files)
--- rest_api/app.js
+++ rest_api/app.js
@@ -9,11 +9,31 @@ const orderRoutes = require('./api/routes/orders');
 //before we use any of those function, we call the morgan first
 app.use(morgan('dev'));
 
-//any incoming request has to go through app.use method
-// next is a function which can be used to move the request to the next middleware inline
-// create a middleware (this is a router)
+/**
+ * any incoming request has to go through app.use method
+ * next is a function which can be used to move the request to the next middleware inline
+ * create a middleware (this is a router)
+ */
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+/* if the request is not meet the requirements in any of the route above, we handle it (the errors) here  all of those router */
+app.use((req, res, next) => {
+  const error = new Error('Sorry the request seems to be not found');
+  error.status = 404;
+  next(error);
+});
 
+/**
+ * Another Error Handlers
+ * 500 : Internal Server Errors
+ */
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 module.exports = app;
