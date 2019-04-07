037fc3bd4e3c0fb489859469171ce9ad01fdb020
WildanKurniadi <danprogramming10@gmail.com>
2019-04-07 20:38:21 +0700

    REST API #4 - Install nodemon and use Morgan for logging the middleware, add some endpoints

--- rest_api/app.js
+++ rest_api/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); //morgan will logs your middleware
 
 //import product.js route
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+//before we use any of those function, we call the morgan first
+app.use(morgan('dev'));
+
 //any incoming request has to go through app.use method
 // next is a function which can be used to move the request to the next middleware inline
-// create a middleware
+// create a middleware (this is a router)
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
 
 module.exports = app;
